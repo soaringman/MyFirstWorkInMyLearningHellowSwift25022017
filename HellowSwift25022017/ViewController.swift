d15178cf85c2fa246d30cdad1130bd57ab184e51
Macuser <macuser@MacBook-Pro-Macuser.local>
2017-02-25 10:13:05 +0300

    Initial Commit

---
+++ HellowSwift25022017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HellowSwift25022017
+//
+//  Created by Macuser on 25.02.17.
+//  Copyright © 2017 Macuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

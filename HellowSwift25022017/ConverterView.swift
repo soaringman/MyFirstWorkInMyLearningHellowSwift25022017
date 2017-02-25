779f70e29e12c3e3b59ef9e050b8592c9792c564
Macuser <macuser@MacBook-Pro-Macuser.local>
2017-02-25 15:19:07 +0300

    implemented ConverterView

--- HellowSwift25022017/ConverterView.swift
+++ HellowSwift25022017/ConverterView.swift
@@ -8,14 +8,50 @@
 
 import UIKit
 
+//что бы задать слабую ссылку зададим наследование нашего протокола от протокола class
+protocol ConverterViewDelegate:class {
+    func converterView(view:ConverterView, convertFroward value:String?)
+    func converterView(view:ConverterView, convertBackward lavue:String?)
+}
 class ConverterView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    weak var delegate:ConverterViewDelegate?
+    //поле слева сделаем сеттер
+    var toValue:String?  {
+        set {
+            toTextField?.text = newValue
+        }
+        get {
+            return toTextField?.text
+        }
     }
-    */
-
+    
+    //поле справа сделаем геттер
+    var fromValue:String?{
+        get {
+            return fromTextField?.text
+        }
+        set {
+            fromTextField?.text = newValue
+        }
+    }
+    //    привяжим два текстовых поля
+    @IBOutlet private weak var toTextField:UITextField?
+    @IBOutlet private weak var fromTextField:UITextField?
+    
+    @IBAction func forwardPressed(){
+        delegate?.converterView(view: self, convertFroward: fromValue)
+    }
+    @IBAction func backwardPressed(){
+        delegate?.converterView(view: self, convertBackward: fromValue)
+    }
+    required init?(coder aDecoder:NSCoder){
+        super.init(coder: aDecoder)
+        setup()
+        
+    }
+    private func setup(){
+        self.layer.cornerRadius = 5
+        self.clipsToBounds = true
+    }
 }
